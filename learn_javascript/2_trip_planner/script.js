1c4d7df62d16b809c5dd21f83b676f87afa944b6
Josselin <julien.josselin75@hotmail.fr>
2025-01-16 16:46:34 +0100

    TripPlanner: store html elements in const

--- learn_javascript/2_trip_planner/script.js
+++ learn_javascript/2_trip_planner/script.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+
+// Premièrement, cibler tous les éléments importants
+// du HTML
+const contentForm = document.getElementById("content-form");
+const contentFormButton = document.getElementById(
+  "content-form-button"
+);
+const contentDisplay = document.getElementById("content-display");
