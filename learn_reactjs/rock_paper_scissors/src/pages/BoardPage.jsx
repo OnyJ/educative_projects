942e33a950ad9fbf53d1239e6ba321e397852fa5
OnyJ <julien.josselin75@hotmail.fr>
2025-01-24 13:50:23 +0100

    Create BoardPage.jsx

---
+++ learn_reactjs/rock_paper_scissors/src/pages/BoardPage.jsx
@@ -0,0 +1,12 @@
+export default function BoardPage() {
+  return (
+    <>
+      <h1>Rock Paper Scissors !</h1>
+
+      <div id="board">
+        
+      </div>
+      <div id="actions"></div>
+    </>
+  );
+}
