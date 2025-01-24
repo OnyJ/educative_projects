1605acffe56f244b4c7637c4350e94d1878717ab
OnyJ <julien.josselin75@hotmail.fr>
2025-01-24 16:07:52 +0100

    Edit AddPlayersPage: handle add player

--- learn_reactjs/rock_paper_scissors/src/pages/AddPlayersPage.jsx
+++ learn_reactjs/rock_paper_scissors/src/pages/AddPlayersPage.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 export default function AddPlayersPage() {
+  const [players, setPlayers] = useState([]);
+
+  function handleAddPlayer(event) {
+    event.preventDefault(); // éviter que la page se recharge
+
+    if (players.length === 2) {
+      console.error("Le nombre max de joueurs a été atteint");
+      return;
+    }
+
+    const playerName = event.target[0].value;
+    const newPlayer = { name: playerName, score: 0, action: "" };
+
+    setPlayers([...players, newPlayer]);
+    event.target.reset();
+
+    if (players.length === 1) {
+      alert("Redirect, game starts !");
+    }
+  }
+
   return (
     <>
       <h1>Rock Paper Scissors !</h1>
       <h2>Add Players Page</h2>
 
-      <form id="add-player-form">
+      <form id="add-player-form" onSubmit={handleAddPlayer}>
         <label for="player-name">Player Name:</label>
         <input type="text" id="player-name" required />
         <br />
@@ -12,9 +35,11 @@ export default function AddPlayersPage() {
       </form>
 
       <div>
+        <h2>Joueurs</h2>
         <ul id="players-list">
-          <li>Exemple 1</li>
-          <li>Exemple 2</li>
+          {players.map((player, index) => (
+            <li key={index}>{player.name}</li>
+          ))}
         </ul>
       </div>
     </>
